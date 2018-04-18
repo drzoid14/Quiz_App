5d2fc6bb053160b79d1f0755b9dc48adfeb2c583
Billy Angermeier <william.r.angermeier@gmail.com>
2018-04-18 18:17:06 -0400

    -m started quiz app, initial commit

---
+++ bank.js
@@ -0,0 +1,92 @@
+const BANK=[
+
+{quest:'Q1: What was the first publication of DC Comics?'
+
+	ans=["Superman",
+	"Action Comics",
+	"New Fun",
+	"Batman"]
+
+feed:"A1: New Fun: The Big Comic Magazine was published in 1935, 3 years before the introduction of Superman in Action Comic"}
+
+
+{quest: "Q2: What was the first Marvel/DC crossover comic?"
+
+	ans=['Superman vs The Amazing Spider-Man',
+	'JLA/Avengers',
+	'Batman and Captain America',
+	]'Green Lanter Silver Surfer: Unholy Alliances'
+
+feed:"A2: Superman vs The Amazing Spider-Man was published in 1976 featuring the most popular superhero from each universe taking on Lex Luthor and Dr. Octopus"}
+
+{quest: 'Q3: Which one of these is NOT a Marvel character?'
+
+	ans=['Howard the Duck', 'Thunder Frog', 'The Asbestos Lady', 'Tank Girl']
+
+	feed:'A3: Tank Girl was originally published by Titan Comics became a licensed property Dark Horse Comics.  Thunder Frog is a frog Thor, The Asbestos Lady is a villain of the Human Torch, and Howard the Duck is...well...Howard the Duck.'}
+
+{quest: "Q4: The MCU is a LARGE universe spanning 10 years and 18 movies (thus far).  In the decade movies, what has been the highest grossing Marvel film?"
+
+	ans = ['Guardians of the Galaxy Vol. 2',
+	'Iron Man',
+	'Black Panther',
+	'The Avengers']
+
+	feed:'A4: The Avengers still holds the crown with $623 Million at the box office.'}
+
+
+{quest: 'Q5: Marvel Comics #1 was published in 1939 and featured the first appearance of which character?'
+
+	ans = ['Captain America',
+	'Wolverine',
+	'Namor, the Sub-Mariner',
+	'Vision']
+
+feed: 'A5: Namor the sub-mariner is introduced along side the Human Torch(in his second appearance). Vision was not introduced until 1940, Captain America in 1941, and Wolverine until 1974.'}
+
+{quest: 'Q6: The X-Men is one of the most popular superhero teams with over 500 issues and over 30 members. Who was not a member of the original team in the 1960s?'
+
+	ans = ['Cyclops',
+	'Marvel Girl',
+	'Storm'
+	'Beast'
+
+feed: 'A6: Storm wasn\'t introduced until 1975.  The original X-Men Roster was Angel, Beast, Cyclops, Iceman, and Marvel Girl.'}
+
+{quest: 'Q7: The Justice League first showed up in the Brave and the Bold issue 28 in 1960. Who was the first villain to take on the League?'
+
+	ans = ['Lex Luthor',
+	'Starro the Conquerer',
+	'The Joker',
+	'Vandal Savage']
+
+feed: 'A7: While each of these Villains took on the League at some point, Starro the conquerer was the first.'}
+
+{quest: 'Q8: The Joker has become one of the most iconic villains in the comic book world.  Who has not portrayed the Joker?'
+
+	ans = ['Mark Hamill',
+	'Zach Galifinakis',
+	'Cesar Romero',
+	'James Franco']
+
+feed: 'A8:  James Franco, while a jokester, has not (yet) played the joker, though many fans feel like he would do the role justice.  Hamill has spent quite a few years voicing the animated joker, Zach Galifinakis was the voice for the Lego Batman movie, and Cesar Romero had one of the first and most iconic versions of the Clown Prince of Crime.'}
+
+{quest: 'Q9: Peter Parker, aka Spider-Man, has been romantically involved with many people in the Marvel Universe throughout his years. Who has he NOT been linked with?'
+
+	ans = ['Black Cat',
+	'Kitty Pryde',
+	'Cara Danvers',
+	'Diana Prince']
+
+feed: 'A9: Diana Prince, aka Wonder Woman, is part of the DC Universe and has not been interested in a relationship with Spider-Man even during the crossover where they appeared together.'}
+
+{quest: 'Q10: The Hulk is seen as a nearly unstoppable force of rage who has appeard with many different heroes in the Marvel Universe. How many issues did his original solo title series have before it was canceled?'
+
+	ans = ['50',
+	'6',
+	'112',
+	'300']
+
+feed: 'A10: 6. Despite his longevity and popularity, his initial solo series was not well received.'}
+
+]
---
+++ index.js
@@ -0,0 +1,152 @@
+let number = 0;
+let score = 0;
+
+//Buttons change screens, hiding the intro, revealing the question,
+//changing to the feedback screen, and restarting the quiz at the 
+//end
+$(function(){
+	$(`.begin`).on('click',(function(event){
+		console.log('hi');
+		$(`.stats`).css('display','flex');
+		$(`.startScreen`).hide();
+		$('body').css('background-image','url(https://static.comicvine.com/uploads/scale_super/13/138994/4129237-marvel_vs_dc_657.jpg');
+		$('body').css('background-color','lightgray');
+		$(`#questionScreen`).html(generateQuestion());
+	}
+	));
+	
+});	
+	
+	//Generate questions in the div
+	function generateQuestion() {
+		if (number < STORE.length) {
+		  $(`#questionScreen`).show();
+		  console.log(`${STORE[number].quest}`);
+			return `<div class = "question"> 
+					<h3 role="question">${STORE[number].quest}</h3>
+					  <div class = "answers">
+						  <form role = "Answer Options" onsubmit="getFeedback()">
+						    <fieldset>
+						      <legend>Answers</legend>
+				  		  	<label class="option">
+					  		  	<input type="radio" id='a' value="${STORE[number].ans[0]}" name="ans" required><span class='button'>${STORE[number].ans[0]}</span>
+				  			  </label></br>
+				  			  <label class="option">
+					  			  <input type="radio" id='b' value="${STORE[number].ans[1]}" name="ans" required><span class='button'>${STORE[number].ans[1]}</span>
+			  				  </label></br>
+				  			  <label class ="option">
+					  			  <input type="radio" id='c' value="${STORE[number].ans[2]}" name="ans" required><span class='button'>${STORE[number].ans[2]}</span>
+				  			  </label></br>
+				  			  <label class = "option">
+		  						  <input type="radio" id='d' value="${STORE[number].ans[3]}" name="ans" required><span class='button'>${STORE[number].ans[3]}</span>
+			  				  </label></br>
+				  				  <button type="submit" class = "submit" >Submit</button>
+				  			</fieldset>  
+			  			</form>
+			  		</div>`	;
+
+		} else {
+		        showFinal();
+		}
+	}
+	
+		
+//Checks users submitted value against the bank of correct Answer		
+function checkAnswer() {
+      let selected = $('input[name=ans]:checked').val();
+      let correct = STORE[number].correct;
+      
+ //checks value of A: B: C: or D: 
+     if(selected.startsWith(correct)===true) {
+    console.log('success');
+    correctAnswer();
+    console.log(score);
+    return `<div class = "right">
+	      		<p>Correct! <span>${STORE[number].feed} </span> Great job!</p>
+	    	</div>
+	    	<div><button type="submit" onclick="nextQuestion()">Next Question</button>`;
+  } else {
+          
+          return `<div class = 'wrong'>
+	     		<p>Sorry! <span>${STORE[number].feed} </span></p>
+	      	</div>
+	      	<div><button type="submit" onclick="nextQuestion()">Next Question</button>`;	
+  }
+  
+  
+}		
+//On submitting answer, changes screens and gives feedback	
+function getFeedback(){
+  event.preventDefault();
+  $('#questionScreen').hide();
+  $(`#feedbackScreen`).css(`display`,`block`);
+   $(`#feedbackScreen`).html(checkAnswer());
+}
+
+//If an answer is correct, increases score and updates header 
+function correctAnswer() {
+      score+=1;
+    $(`#correct`).text(score);
+}
+
+//moves to next question, hides feedback, updates header
+function nextQuestion() {
+  number+=1;
+  $(`#number`).text(number+1);
+	$(`#questionScreen`).html(generateQuestion());
+  $(`#feedbackScreen`).hide();
+}		
+
+//Gives final feedback
+function showFinal() {
+  console.log('finalScreen ran');
+  $(`.finalScreen`).css(`display`,`block`);
+  $(`.finalScreen`).html(showResults());
+}	
+
+
+//Gives the final score on final screen
+function showResults() {
+  $(`.stats`).hide();
+  $(`#feedbackScreen`).hide();
+  $(`#finalScore`).text(score);
+  $(`.results`).css('display','block');
+  if(score>7) {
+    return `<div class = 'great'> <h3>Great Job! Seeing as you know your random comic book knowledge, I hope you enjoyed the quiz!</h3></div>
+    <div class = 'button'><button type = "submit" onclick="restartQuiz()">Try Again</button></div>
+    `;
+  } else if(score>5) {
+    return `<div class = 'fair'> <h3>Not Bad! You probable have some knowledge, at least enough to get more than half right. I hope you learned something new and interesting today!</h3></div>
+        <div class = 'button'><button type = "submit" onclick="restartQuiz()">Try Again</button></div>
+    `;
+  } else {
+    return `<div class = 'poor'> <h3>Good Try! Not everyone is a nerd, and that's OK.  I hope you had fun and learned something new today!</h3></div>
+        <div class = 'button'><button type = "submit" onclick="restartQuiz()">Try Again</button></div>
+    `;
+  }
+}
+
+//Resets score to 0, updates header
+function resetScore(){
+  score=0;
+  $(`#correct`).text(score);
+}
+
+
+//restarts questions, updeates header
+function resetNumber(){
+  number=0;
+  $(`#number`).text(number+1);
+}
+
+//Restarts the quiz, returns to start page
+function restartQuiz() {
+  resetNumber();
+  resetScore();
+	$(`.startScreen`).show();
+	$('body').css('background-image','url(https://i.pinimg.com/originals/0d/7d/ea/0d7dea8e2f40c7abe12295a63b31c1d3.jpg');
+	$('.finalScreen').hide();
+	$('.results').hide();
+  $(`.stats`).hide();
+}
+
